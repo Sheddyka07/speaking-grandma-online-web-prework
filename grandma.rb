006ec8968986cdd1ff184ff7fa1a84e3e60ea07a
Kelly Sheddy <jungaka07@gmail.com>
2018-09-29 19:21:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   if phrase == '#{phrase}'.upcase
-    puts ""
+    puts "NO, NOT SINCE 1938!"
+    
   
   puts "HUH?! SPEAK UP, SONNY!"
 elsif greeting == statement.upcase
