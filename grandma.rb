a2fa79b315a9377784928eae3c01491a4d771cee
Kelly Sheddy <jungaka07@gmail.com>
2018-09-29 19:18:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 
-def speak_to_grandma(greeting)
+def speak_to_grandma(statement)
   statement = "#{greeting}"
 end
 
-if statement == statement.downcase
+if greeting == statement.downcase
   puts "HUH?! SPEAK UP, SONNY!"
-elsif statement == statement.upcase
+elsif greeting == statement.upcase
   puts "NO, NOT SINCE 1938!"
-elsif statement == "I LOVE YOU GRANDMA!"
+elsif greeting == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 else puts "HUH?! SPEAK UP, SONNY!"
 end
