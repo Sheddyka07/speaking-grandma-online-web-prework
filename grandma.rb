18271bc11a300fc1bac437ee3f3a513d4ad7d521
Kelly Sheddy <jungaka07@gmail.com>
2018-09-29 19:14:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,13 +3,5 @@ def speak_to_grandma(greeting)
   statement = "#{greeting}"
 end
 
-if statement = "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif statement = "Hi Nana, how are you?"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif statement = "Hi!"
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif statement = "WHAT DID YOU EAT TODAY?"
-  puts "NO, NOT SINCE 1938!"
-else puts "HUH?! SPEAK UP, SONNY!"  
-end  
+if statement = statement.upcase
+  puts ""
