01fc5cf8d76f6cf9d4a0b6c82a6af964175cb6bb
Kelly Sheddy <jungaka07@gmail.com>
2018-09-29 19:01:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(greeting)
+  statement = "#{greeting}"
+end
 
+
+  
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
