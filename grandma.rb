9cf4c9c8c3cfae0dc3e742fec3318b17f99706fb
Kelly Sheddy <jungaka07@gmail.com>
2018-09-29 19:09:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,9 @@ if statement = "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif statement = "Hi Nana, how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
+elsif statement = "Hi!"
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif ""  
   
   
 # Whatever you say to grandma, she should respond with
